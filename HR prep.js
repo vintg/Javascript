2532c020f0363475f84bcee961d6ca502b5e08c7
libertysky <liberty.sky87@gmail.com>
2018-06-19 18:20:33 -0400

    almost done with mod 2

--- HR prep.js
+++ HR prep.js
@@ -1706,3 +1706,64 @@ function assertEqual(actual, expected, testName) {
 assertEqual(findLongestPalindrome('hihi whatahw issi thissiht ??????????????'), 'thissiht', 'longest palindrome in sentence');
 
 //017
+function designerShoes (designer, shoes){
+  return shoes.map(function(shoe){
+    return [designer, shoe.name, shoe.price].join(', ');
+  });
+}
+
+function genDesc(shoeList){
+  return designerShoes(shoeList.name, shoeList.shoes).join('\n');
+}
+
+function genInventory(inventory){
+  return inventory.map(function(item){
+    return genDesc(item);
+  });
+}
+
+function assertEqual(actual, expected, testName) {
+  actual = JSON.stringify(actual);
+  expected = JSON.stringify(expected);
+  if (actual === expected) {
+    console.log('passed [' + testName + ']');
+  } else {
+    console.log('FAILED [' + testName + '] Expected "' + expected + '", but got "' + actual + '"');
+  }
+}
+
+var currentInventory = [
+  {
+    name: 'Brunello Cucinelli',
+    shoes: [
+      {name: 'tasselled black low-top lace-up', price: 1000},
+      {name: 'tasselled green low-top lace-up', price: 1100},
+      {name: 'plain beige suede moccasin', price: 950},
+      {name: 'plain olive suede moccasin', price: 1050}
+    ]
+  },
+  {
+    name: 'Gucci',
+    shoes: [
+      {name: 'red leather laced sneakers', price: 800},
+      {name: 'black leather laced sneakers', price: 900}
+    ]
+  }
+];
+
+var one = currentInventory[0];
+var designer = one.name;
+var shoes = one.shoes;
+var right = ["Brunello Cucinelli, tasselled black low-top lace-up, 1000", "Brunello Cucinelli, tasselled green low-top lace-up, 1100", "Brunello Cucinelli, plain beige suede moccasin, 950", "Brunello Cucinelli, plain olive suede moccasin, 1050"];
+
+assertEqual(designerShoes(designer,shoes), right, 'generate designer + shoes array');
+
+right = "Brunello Cucinelli, tasselled black low-top lace-up, 1000\nBrunello Cucinelli, tasselled green low-top lace-up, 1100\nBrunello Cucinelli, plain beige suede moccasin, 950\nBrunello Cucinelli, plain olive suede moccasin, 1050";
+
+assertEqual(genDesc(one), right, 'generate newline list descriptions');
+
+right = ["Brunello Cucinelli, tasselled black low-top lace-up, 1000\nBrunello Cucinelli, tasselled green low-top lace-up, 1100\nBrunello Cucinelli, plain beige suede moccasin, 950\nBrunello Cucinelli, plain olive suede moccasin, 1050", "Gucci, red leather laced sneakers, 800\nGucci, black leather laced sneakers, 900"];
+         
+assertEqual(genInventory(currentInventory), right, 'generate descriptions for a given inventory list');
+
+//018
