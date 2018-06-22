0eeb8cd44c91146a8331630e5a18ef4de4cb6a4a
libertysky <liberty.sky87@gmail.com>
2018-06-22 15:38:38 -0400

    003

--- HR prep mod 3.js
+++ HR prep mod 3.js
@@ -26,3 +26,52 @@ function assertEqual(actual, expected, testName){
 }
 
 assertEqual(output,expected, 'flip position of every adjacent pair of characters in a string');
+
+//002 Big Flipper
+function assertValidInput(string, n){
+  return n>1 && typeof string ==='string';
+}
+
+function reverseString(str) {
+    return str.split('').reverse().join('');
+}
+
+function flipEveryNChars(string, n){
+  var m = string.length;
+  var r = m%n;
+  
+  var out= '';
+  for(var i = 0; i<m;i+=n){
+     out+= reverseString(string.slice(i,i+n)); 
+  }
+  out+= string.slice(m-r,m);
+  return out;
+}
+
+function assertEqual(actual, expected, testName){
+  if (actual===expected){
+    console.log('PASSED');
+  } else { 
+    console.log('FAILED [' + testName + '] expected: \n'+expected + '\n but got:\n'+actual);
+  }
+}
+
+// TEST INPUT
+var input = 'a short example'; 
+var n = [3,5];
+var expected = ['s arohe tmaxelp', 'ohs axe trelpma']; 
+//
+for (var _ in n){
+  main(input, n[_], expected[_]);
+}
+
+function main(input, n, expected){
+  if (assertValidInput(input,n)){
+    var output = flipEveryNChars(input, n);
+    assertEqual(output, expected, 'reverse every n char in a string');
+  } else{
+    console.log('Input(s) not valid');
+  }
+}
+
+//003
