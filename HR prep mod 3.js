9197d78bb8f4396035233845de32b94100893c48
libertysky <liberty.sky87@gmail.com>
2018-06-22 21:45:29 -0400

    arrays ftw

--- HR prep mod 3.js
+++ HR prep mod 3.js
@@ -75,3 +75,54 @@ function main(input, n, expected){
 }
 
 //003
+evens = "2 4 7 8 10"; // => 3 - Third number is odd, while the rest of the numbers are even
+
+odds = "1 10 1 1";  //=> 2 - Second number is even, while the rest of the numbers are odd
+
+var actual = detectOutlierValue(evens);
+expected = 3;
+assertEqual(actual, expected, 'find index of minority even-odd number in string');
+
+actual = detectOutlierValue(odds);
+expected = 2;
+assertEqual(actual, expected, 'find index of minority even-odd number in string');
+
+function assertEqual(actual, expected, testName){
+  if (actual===expected){
+    console.log('PASSED');
+  } else {
+    console.log ('FAILED [' + testName + '] expected: \n'+ expected + '\bbut got:\n'+actual);
+  }
+}
+
+function detectOutlierValue(numString){
+  var numbers = numString.split(' ');
+  var type = majorityType(numbers);
+  return oddNumberOut(numbers, type);
+}
+
+function majorityType(numbers){
+  var ct = 0;
+  for (var n =0 ;n<3; n++){
+    ct+= isEven(Number(numbers[n]));
+  }
+  return ct>1 ? 0: 1; 
+}
+
+function isEven(number){
+  return Math.abs(number)%2==0;
+}
+
+function oddNumberOut(numbers, type){
+  var idx = 1;
+  for (var i in numbers){
+    var n = Number(numbers[i]);
+    
+    if (type===1 && isEven(n)===true){
+        return idx;
+    } else if (type===0 && isEven(n)===false){
+        return idx;
+    }
+    idx++;
+  }
+}
