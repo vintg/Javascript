80bcf21c73c9ba3113b3b979737d43bd3e48921a
libertysky <liberty.sky87@gmail.com>
2018-06-27 12:00:45 -0400

    updated 3.001

--- HR prep mod 3.js
+++ HR prep mod 3.js
@@ -1,31 +1,37 @@
 //MODULE 3 
 //001 Flipper
-function flipPairs(input){
-  var str = '';
-  for (var i =1; i<input.length;i+=2){
-    str+=input[i];
-    str+=input[i-1];
-  }
-  if (input.length%2!=0){ str+=input[input.length-1];}
-  return str;
+function flipPairs(string){
+  var len = string.length;
+  var out = '';
+  
+  for (var i=0; i<len;i+=2){
+    if (string[i+1]!=undefined){
+              out+=string[i+1];}
+    out+=string[i];
+    }
+  
+  if (len!=0){
+    //add last char
+    out+=string.slice(-1,1);//out+= string[len-1]; 
+  } 
+  
+  return out;
 }
 
-//Example:
+function assertEqual (actual, expected, testName){
+  if (actual===expected){
+    console.log('PASSED');
+  }
+  else{ 
+  console.log('FAILED ['+testName+'] expected:\n'+expected+'\nbut got: \n'+actual);
+  }
+}
+
 var input = 'check out how interesting this problem is, it\'s insanely interesting!';
 var output = flipPairs(input);
-console.log(output); // --> 
 
 var expected = 'hcce kuo toh wnietertsni ghtsip orlbmei ,si \'t sniasenyli tnreseitgn!';
-
-function assertEqual(actual, expected, testName){
-  if (actual===expected){
-    console.log ('PASSED');
-  } else{
-    console.log('FAILED [' + testName + '] -> expected: \n'+expected + '\nbut got: \n'+actual);
-  }
-}
-
-assertEqual(output,expected, 'flip position of every adjacent pair of characters in a string');
+assertEqual (output, expected, 'flip every pair of char in a string');
 
 //002 Big Flipper
 function assertValidInput(string, n){
